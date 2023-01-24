8b9146b4793c03ffce5d9de2df768b9bf5ff4866
karel rehor <karl.koerner@bonitoo.io>
2023-01-24 17:12:33 +0100

    chore: WIP further tidying and rearrange tests

--- src/test/java/org/kagaka/cell/CellTest.java
+++ src/test/java/org/kagaka/cell/CellTest.java
@@ -16,40 +16,4 @@ class CellTest {
         vc.getTransform().doIt();; 
     }
     
-    
- /* 
-  * TODO move these tests to lifeToy project
-  *   
-    @Test
-    void simpleLifeVCellTest() {
-        VertexImpl<VertexCell> vertex = new VertexImpl<VertexCell>();
-        SimpleLifeVCell slvc = new SimpleLifeVCell(null);
-        VertexCellFactory.joinVertexCell(slvc, vertex);
-        assertEquals(slvc.getVertex(), vertex);
-        assertEquals(vertex.get(), slvc);
-        slvc.doTransform();
-        slvc.getTransform().doIt();
-        System.out.println("");
-        assertFalse(slvc.isAlive());
-    }
-    
-    @Test
-    void pairLifeVCellTest() {
-        VertexImpl<VertexCell> v1 = new VertexImpl<VertexCell>();
-        VertexImpl<VertexCell> v2 = new VertexImpl<VertexCell>();
-        SimpleLifeVCell c1 = new SimpleLifeVCell(null);
-        SimpleLifeVCell c2 = new SimpleLifeVCell(null);
-        VertexCellFactory.joinVertexCell(c1, v1);
-        VertexCellFactory.joinVertexCell(c2, v2);
-        v1.addMutualEdge(v2);
-        System.out.println("Transforming C1");
-        c1.doTransform();
-        System.out.println("Transforming C2");
-        c2.doTransform();
-        System.out.println("");
-        assertEquals(1, c1.getVertex().getEdges().size());
-        assertEquals(1, c2.getVertex().getEdges().size());
-    }
-    */
-
 }
