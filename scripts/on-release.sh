e00097cd1452bbc5761408aed443c666b25179eb
karel rehor <karl.koerner@bonitoo.io>
2026-07-30 16:45:48 +0200

    chore: troubleshoot - calling bash script 03

--- scripts/on-release.sh
+++ scripts/on-release.sh
@@ -5,9 +5,10 @@ echo "Starting ${0}"
 if [ -n "${GITHUB_ACTIONS}" ]
 then
   echo "Running in Github Actions container"
-  echo "DEBUG tag name #${{ github.event.release.tag_name }}#"
 else
   echo "Running locally"
 fi
+
+echo "DEBUG tag name #${RELEASE_TAG}#"
 
 env
