2b83aa21baf505d52f1eaeb6fe12090d8bd540e8
karel rehor <karl.koerner@bonitoo.io>
2026-07-31 14:47:58 +0200

    chore: prepare on-release.sh 01

--- scripts/on-release.sh
+++ scripts/on-release.sh
@@ -8,6 +8,14 @@ then
   echo "Local runs are not yet supported."
   exit 1
 fi
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PROJECT_DIR="${SCRIPT_DIR}/.."
+CHANGELOG_PATH="${PROJECT_DIR}/CHANGELOG.md"
+RELEASE_NUM=""
+NEXT_RELEASE_NUM=""
+NEXT_RELEASE_BRANCH_BASE="chore/prepare-next-release-"
+RC_OR_BETA=false
+IS_SNAPSHOT=false
 
 FAILURE_BOILERPLATE="Please delete the tag ${CIRCLE_TAG} and the related release, and start again."
 
@@ -52,8 +60,59 @@ setup(){
   fi
 }
 
+set_release_number(){
+    RELEASE_NUM=$(echo ${RELEASE_TAG_NAME} | sed -r "s/-(rc|beta)[0-9]+//" | sed -r "s/^v//")
+}
+
+verify_rc_or_beta(){
+  if [  -n "$(echo ${RELEASE_TAG_NAME} | grep -E "rc|beta")" ]
+  then
+    RC_OR_BETA=true
+  fi
+}
+
+verify_changelog() {
+
+  CHANGELOG_RELEASE_HEADERS=$(sed -n '/^#.*[0-9].[0-9]*.[0-9].*/p' "${CHANGELOG_PATH}")
+
+  mapfile -t HEADER_ARRAY <<< "$CHANGELOG_RELEASE_HEADERS"
+
+  mapfile -td ' ' HEADER_LINE <<< "${HEADER_ARRAY[0]}"
+
+  HEADER_TAG="${HEADER_LINE[1]}"
+  HEADER_DATE="${HEADER_LINE[2]:1:-2}"
+
+  if [ "$HEADER_TAG" != "$RELEASE_NUM"  ]; then
+    printf "ERROR: Latest HEADER_TAG in CHANGELOG.md (%s) does not match release number (%s) from git tag (%s)\n" \
+    "$HEADER_TAG" \
+    "$RELEASE_NUM" \
+    "$CIRCLE_TAG"
+    printf "Please update the latest HEADER_TAG in CHANGELOG.md\n"
+    printf "%s" "${FAILURE_BOILERPLATE}"
+    exit 1
+  else
+    printf "CHANGELOG.md release number check: OK ✓\n"
+  fi
+
+  if [[ ! "$HEADER_DATE" =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}$ ]]; then
+    printf "ERROR invalid commit date (%s) in last CHANGELOG.md entry\n" "$HEADER_DATE"
+    printf "Please update the commit date in CHANGELOG.md\n"
+    printf "%s\n" "${FAILURE_BOILERPLATE}"
+    exit 1
+  else
+    printf "CHANGELOG.md release date check: OK ✓\n"
+  fi
+}
+
 echo "Running in Github Actions container."
+
 github_check
+
+verify_rc_or_beta
+set_release_number
+
+verify_changelog
+
 setup
 
 echo "====== DEBUG ENV ====="
