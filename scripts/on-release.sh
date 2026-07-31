7c5eedbadb65bcb2056fd346bf11df48caa6724b
karel rehor <karl.koerner@bonitoo.io>
2026-07-31 13:06:03 +0200

    chore: start tailoring on-release script 02

--- scripts/on-release.sh
+++ scripts/on-release.sh
@@ -2,6 +2,15 @@
 
 echo "Starting ${0}"
 
+if [ -z "${GITHUB_ACTIONS}" ]
+then
+  echo "This script can only be run in a Github action container."
+  echo "Local runs are not yet supported."
+  exit 1
+fi
+
+FAILURE_BOILERPLATE="Please delete the tag ${CIRCLE_TAG} and the related release, and start again."
+
 github_check(){
 
   if [ "${GITHUB_EVENT_NAME}" == "workflow_dispatch" ]; then
@@ -23,17 +32,30 @@ github_check(){
   echo "Running ${GITHUB_EVENT_NAME} with tag ${RELEASE_TAG_NAME}."
 }
 
+setup(){
+  ADDITIONAL_INSTALLS=""
+  if ! [ -x "$(command -v xmllint)" ]
+  then
+    ADDITIONAL_INSTALLS="${ADDITIONAL_INSTALLS} libxml2-utils"
+  else
+    printf "have xmllint\n"
+  fi
 
-if [ -n "${GITHUB_ACTIONS}" ]
-then
-  echo "Running in Github Actions container."
-  github_check
-else
-  echo "This script can only be run in a Github action container."
-  echo "Local runs are not yet supported."
-  exit 1
-fi
+  if [ -n "${ADDITIONAL_INSTALLS}" ]
+  then
+    printf "This script requires the following unavailable libraries: %s\n" "${ADDITIONAL_INSTALLS}"
+    printf "Please install them and then continue.\n"
+    printf "%s\n" "${FAILURE_BOILERPLATE}"
+    exit 1
+  else
+    printf "Additional requirements already satisfied.\n"
+  fi
+}
 
-echo "======"
+echo "Running in Github Actions container."
+github_check
+setup
+
+echo "====== DEBUG ENV ====="
 
 env
