b377b168aad33df8d22f7c340ccc938e517ecc90
karel rehor <karl.koerner@bonitoo.io>
2026-08-03 13:15:28 +0200

    chore: add pre_release var check 02.

--- scripts/on-release.sh
+++ scripts/on-release.sh
@@ -26,6 +26,7 @@ FAILURE_BOILERPLATE="Please delete the tag ${CIRCLE_TAG} and the related release
 
 echo "DEBUG GITHUB_REF_NAME #${GITHUB_REF_NAME}#"
 echo "DEBUG IS_PRERELEASE #${IS_PRERELEASE}#"
+echo "DEBUG AUTHOR #${RELEASE_AUTHOR}#"
 
 github_check(){
 
