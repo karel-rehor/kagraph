2473e453d994aa186d598db083fa5ef383aed5c3
karel rehor <karl.koerner@bonitoo.io>
2026-09-15 13:22:27 +0200

    chore: change case when testing for RC or BETA.

--- scripts/experim.sh
+++ scripts/experim.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
-RC_OR_BETA=true
+RC_OR_BETA=false
+
+
+echo "DEBUG TEST_TAG_NAME ${TEST_TAG_NAME}"
+
+LOWER_TAG_NAME=$(echo "${TEST_TAG_NAME}" | tr '[:upper:]' '[:lower:]')
+
+echo "DEBUG LOWER_TAG_NAME ${LOWER_TAG_NAME}"
+
+if echo "${LOWER_TAG_NAME}" | grep -q "rc|beta"
+then
+  RC_OR_BETA=false
+fi
 
 export RC_OR_BETA
